3f7b098804df9d6f224bc16866839f06db47246c
keenn198 <keenn198@potsdam.edu>
2018-07-13 15:17:50 -0400

    removed folder from project

--- Pong/End.gd
+++
@@ -1,7 +0,0 @@
-extends TextureFrame
-func _ready():
-	pass
-func _on_Yes_pressed():
-	get_tree().change_scene("res://Pong.tscn")
-func _on_No_pressed():
-	get_tree().quit()
--- Pong/Menu.gd
+++
@@ -1,5 +0,0 @@
-extends TextureFrame
-func _ready():
-	pass
-func _on_Start_pressed():
-	get_tree().change_scene("res://Pong.tscn")
